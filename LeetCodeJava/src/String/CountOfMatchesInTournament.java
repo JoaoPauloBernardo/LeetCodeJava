49650aca67816704e076f0de8992c476768717d9
João Paulo Bernardo <99024136+JoaoPauloBernardo@users.noreply.github.com>
2024-05-02 10:19:25 -0300

    Add files via upload

--- LeetCodeJava/src/String/CountOfMatchesInTournament.java
+++ LeetCodeJava/src/String/CountOfMatchesInTournament.java
@@ -1,5 +1,9 @@
 package String;
 
 public class CountOfMatchesInTournament {
-    
+        public int numberOfMatches(int n) {
+            //o numero de partidas jogadas e SEMPRE um numero menor que o numero de times
+            //dessa maneira, nao ha necessidade de um codigo complexo, as vezes, menos é mais mesmo
+            return n - 1;
+        }
 }
