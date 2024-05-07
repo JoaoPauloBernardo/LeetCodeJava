37dd1b583f79b0caf99253b645c6696681a5986e
João Paulo Bernardo <99024136+JoaoPauloBernardo@users.noreply.github.com>
2024-05-06 21:06:35 -0300

    Add files via upload

--- LeetCodeJava/src/Array/BestTimeToBuyAndSellStockWithCooldown.java
+++ LeetCodeJava/src/Array/BestTimeToBuyAndSellStockWithCooldown.java
@@ -1,5 +1,15 @@
 package Array;
 
 public class BestTimeToBuyAndSellStockWithCooldown {
-    //inicializando os placeholders pros lucros maximos
+    public int maxPorfit(int[] prices) {
+        int venda = 0, ontem_venda = 0, comprar = Integer.MIN_VALUE, ontem_compra;
+        for (int price : prices) {
+            ontem_compra = comprar;
+           comprar = Math.max(ontem_venda - price, ontem_compra);
+            ontem_venda = venda;
+            venda = Math.max(ontem_compra + price, ontem_venda);
+        }
+        return venda;
+    }
 }
+
